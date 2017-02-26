6b234581609b168a0ccc9979049334ad1580ae71
Alexey Zotov <lexzotov@gmail.com>
2017-02-26 17:50:57 +0700

    ~ Refactoring

--- src/main/java/com/lex4hex/securedShop/security/MethodSecurityConfig.java
+++ src/main/java/com/lex4hex/securedShop/security/MethodSecurityConfig.java
@@ -2,7 +2,6 @@ package com.lex4hex.securedShop.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
@@ -11,11 +10,11 @@ import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecur
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+
   @Autowired
   private OAuth2SecurityConfiguration securityConfig;
 
   @Override
-  @Lazy
   protected MethodSecurityExpressionHandler createExpressionHandler() {
     return new OAuth2MethodSecurityExpressionHandler();
   }
